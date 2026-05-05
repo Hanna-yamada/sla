64061f9a8e9a1e723e90cc6d46df02ca34146c42
Hanna <20241pvai0030037@estudantes.ifpr.edu.br>
2026-05-05 07:22:42 -0300

    Criar treino.py

---
+++ treino.py
@@ -0,0 +1,39 @@
+# classe jogador
+class Jogador:
+    def __init__ (self):
+        self.nome_jogador = ""
+        self.nick = ""
+        self.apelido = ""
+
+    def cadastro_jogadores (self):
+        print(f"Jogador {self.nome_jogador}: adicionado")
+
+
+    def procurar_nick (self):
+            print(f"Nome do Jogador: {self.nome_jogador} / Nick: {self.nick} / Apelido: {self.apelido}")
+
+class Equipe:
+    def __init__ (self):
+        self.nome_equipe = ""
+        self.jogo = ""
+        self.jogadores = [] 
+
+
+    def cadastro_equipe (self):
+        print(f"Equipe: {self.nome_equipe} adicionado")
+
+
+    def adicionar_jogador(self,jogador):
+        self.jogadores.append(jogador)
+        print(f"Jogador: {jogador.nome_jogador} adicionado na equipe: {self.nome_equipe}")
+
+
+    def listar_equipes (self):
+              print(f"Equipe: {self.nome_equipe}  Jogo: {self.jogo}  Nº Jogadores: {len(self.jogadores)} ")
+
+    def consultar_equipes(self):
+        if len(self.jogadores) == 0:
+            print("Nenhum jogador na equipe.")
+        else:
+            for j in self.jogadores:
+                print(f"Nome: {j.nome_jogador} / Nick: {j.nick} / Apelido: {j.apelido}")
