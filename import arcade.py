7c2a28759a4cb7623a28375708d91056f0395aed
unknown <20241pvai0030037@estudantes.ifpr.edu.br>
2026-06-09 10:01:00 -0300

    gamerrr

--- import arcade.py
+++ import arcade.py
@@ -1,36 +1,135 @@
+# pip install arcade no terminal
 import arcade
-ALTURA=600
-LARGURA=800
-TITULO=" P"
+
+ALTURA = 600
+LARGURA = 800
+TITULO = "Meu Joguinho"
 
 class Player(arcade.Sprite):
+
     def __init__(self):
-        super().__init__("pngegg.png", scale=0.3)
-        self.textuta_direita = arcade.load_texture("pngegg.png")
-        self.textuta_esquerda = arcade.load_texture("pngeggg.png")
-    def update(self):
-        pass
+        super().__init__("per.png",scale=0.3)
+        self.textura_direita = arcade.load_texture("per.png")
+        self.textura_esquerda = arcade.load_texture("per2.png")
+        
+
+    def update(self,delta_time):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
 
 
-class JanelaJogo(arcade.Window):
+        if self.change_x > 0:
+           self.textura_direita = self.textura_direita
+        elif self.change_x < 0:
+            self.textura_esquerda = self.textura_esquerda
+
+        if self.right > LARGURA:
+            self.change_x = 0
+            self.right = LARGURA    
+        
+        if self.left < 0:
+            self.change_x = 0
+            self.left = 0
+
+
+        if self.top > ALTURA:
+            self.change_y = 0
+            self.top = ALTURA    
+    
+        if self.bottom < 0:
+            self.change_x = 0
+            self.bottom = 0  
+
+class Moeda(arcade.Sprite):
     def __init__(self):
-        super().__init__(800,600," p")
-        arcade.set_background_color(arcade.color.AMAZON)
+        super().__init__("moeda.png",scale=0.1)
+        self.textura_direita = arcade.load_texture("moeda.png")
+        
 
-        self.personagem = Player()
-        self.personagem.center_x= 400
-        self.personagem.center_y= 300
+    def update(self,delta_time):
+     
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.right > LARGURA or  self.left < 0:
+            self.change_x *= -1
+
+        if self.top > ALTURA or self.bottom < 0:
+            self.change_y *= -1
+
+       
+
+
+
+class MeuJogo(arcade.Window):
+    def __init__(self):
+        super().__init__(800,600,"Meu Joguinho")
+        self.movimento = 3
+        
+
+
+        arcade.set_background_color((237, 111, 111))
+        self.jogador = Player()
+        self.jogador.center_x = 400
+        self.jogador.center_y = 320
+
+        self.sprite_jogador = arcade.SpriteList()
+        self.sprite_jogador.append(self.jogador)
+
+        self.moeda_jogo = Moeda()
+        self.moeda_jogo.center_x = 280
+        self.moeda_jogo.center_y = 100
+        self.moeda_jogo.change_x = self.movimento
+        self.moeda_jogo.change_y= self.movimento
+
+        self.sprite_moeda_jogo = arcade.SpriteList()
+        self.sprite_moeda_jogo.append(self.moeda_jogo)
 
     def on_draw(self):
         self.clear()
-        arcade.draw_sprite(self.personagem)
+        self.sprite_jogador.draw()
+        self.sprite_moeda_jogo.draw()
+    
 
     def on_update(self, delta_time):
-        pass
+        self.sprite_jogador.update(delta_time)
+        self.sprite_moeda_jogo.update(delta_time)
 
-def sla():
-    tela = JanelaJogo()
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.RIGHT:
+            self.jogador.change_x += self.movimento
+
+        elif key == arcade.key.LEFT:
+            self.jogador.change_x -= self.movimento
+
+        elif key == arcade.key.UP:
+            self.jogador.change_y += self.movimento 
+
+        elif key == arcade.key.DOWN:
+            self.jogador.change_y -= self.movimento        
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.RIGHT or key == arcade.key.LEFT:
+            self.jogador.change_x =0
+        elif key == arcade.key.UP or key == arcade.key.DOWN:
+            self.jogador.change_y =0  
+        
+
+
+
+        
+
+
+
+
+def executar():
+    tela = MeuJogo()
     arcade.run()
+    
 
-if __name__=="__main__":                       
-    sla()
+if __name__ == "__main__":
+    executar()
+
+
+
+        
